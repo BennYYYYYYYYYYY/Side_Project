74d474f68d02aa633a44ec2ed950d5ff03ac7a99
HUANG HSIANG <129297292+BennYYYYYYYYYYY@users.noreply.github.com>
2024-07-14 19:57:25 +0800

    Update 2.爬蟲練習.py

--- 專案前練習/2.爬蟲練習.py
+++ 專案前練習/2.爬蟲練習.py
@@ -5,7 +5,7 @@ import csv
 url = 'https://www.ptt.cc/bbs/Stock/index7321.html'
 
 headers = {
-    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+    'user_agent': ''
 }
 
 res = requests.get(url, headers=headers)
